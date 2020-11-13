c2c44b23eafd107537f52d8b97a8f7c1e7954c6a
felix-ping <felix-ping@qq.com>
2020-11-13 18:00:40 +0800

    官网架构完成

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,18 @@
 
 <script>
 import {provide,ref} from 'vue'
+import {router} from './router'
 export default {
   name: 'App',
   setup(){
     const clientWidth = document.documentElement.clientWidth
-    const asideVisible = ref(clientWidth>=500?true:false)
+    const asideVisible = ref(clientWidth >= 500)
     provide('asideVisible',asideVisible)
+    router.afterEach(()=>{
+      if(clientWidth<=500){
+        asideVisible.value = false;
+      }
+    })
   }
 }
 </script>
---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,105 @@
+<template>
+<div> <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p>
+  <p>Switch 的文档</p></div>
+</template>
+
+<script>
+export default {
+name: "switchDemo"
+}
+</script>
+
+<style scoped>
+
+</style>
