b9266804bd92365d1edea0da4368004a70c8addd
felix-ping <felix-ping@qq.com>
2020-11-13 18:46:17 +0800

    完成switch组件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,105 +1,41 @@
+
+
 <template>
-<div> <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p>
-  <p>Switch 的文档</p></div>
+  <div>
+    <Switch v-model:value="switchDefault" ></Switch>
+  </div>
 </template>
 
 <script>
+import Switch from '../lib/Switch.vue'
+import {ref} from 'vue'
 export default {
-name: "switchDemo"
+  name: "switchDemo",
+  components: {Switch},
+  setup(){
+    const switchDefault =ref(true)
+    return {switchDefault: switchDefault}
+  }
 }
 </script>
 
 <style scoped>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 </style>
---
+++ src/lib/Switch.vue
@@ -0,0 +1,52 @@
+<template>
+  <button @click="toggleSwitch" :class="{checked:value}"><span></span></button>
+</template>
+
+<script>
+export default {
+  name: "Switch",
+  props:{value:Boolean},
+  setup(props,context) {
+    const toggleSwitch = () => {
+      context.emit('update:value',!props.value)
+    }
+    return { toggleSwitch}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+$padding: 2px;
+button {
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: grey;
+  border-radius: $h/2;
+  position: relative;
+
+}
+
+span {
+  position: absolute;
+  top: $padding;
+  left: $padding;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h2 / 2;
+  transition: left 250ms;
+}
+
+button.checked{
+  background-color: blue;
+  &>span {
+    left: calc(100% - #{$h2} - #{$padding});
+  }
+}
+button:hover{
+  outline:none
+}
+</style>
